31f77e5ae0af5ae3ac61c7a69f888c3718808acb
Han V <hvakil@olin.edu>
2022-10-24 16:53:24 -0400

    making test run in different dir

--- testing/unit_tests/pre-commit_unit_test.sh
+++ testing/unit_tests/pre-commit_unit_test.sh
@@ -5,3 +5,4 @@
 #add tests to run here
 
 python3 -m pytest "testing/unit_tests/test_dummy.py"
+python3 -m pytest "testing/unit_tests/test_exp_api_work.py"
---
+++ testing/unit_tests/test_exp_api_work.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+import sys
+
+noodlemaps_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(noodlemaps_folder)
+from src import exp_api_work
+
+def test_says_something():
+    response = exp_api_work.say_something()
+    assert response == "something"
